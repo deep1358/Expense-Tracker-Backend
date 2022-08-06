1eceeff94ae6f168654aa003f25340bbdcd0448c
Deep Shah <deepshah1358@gmail.com>
2022-08-06 17:05:35 +0530

    Temp

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(
 			maxAge: 1000 * 60 * 60 * 24 * 30,
 			sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
 			secure: process.env.NODE_ENV === "production",
-			httpOnly: process.env.NODE_ENV === "production",
+			httpOnly: !process.env.NODE_ENV === "production",
 		},
 	})
 );
