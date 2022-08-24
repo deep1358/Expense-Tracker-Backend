6d9207c0cec53e1dd3805f695681a7f6221174fd
Deep Shah <deepshah1358@gmail.com>
2022-08-24 23:50:30 +0530

    Removed session and passport

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,9 @@
-const passport = require("passport");
 const AuthMiddleware = require("../middlewares/auth");
 
 const router = require("express").Router();
 
-// Google authentication route (redirects to Google for authentication)
-router.get(
-	"/google",
-	passport.authenticate("google", {
-		scope: ["profile", "email"],
-	})
-);
-
-//  Google authentication callback route (redirects to frontend)
-router.get(
-	"/google/callback",
-	passport.authenticate("google", {
-		successRedirect: process.env.FRONTEND_URL,
-	})
-);
-
 // Get user data from session
-router.get("/user", require("../controllers/auth/getUser"));
+router.post("/user", require("../controllers/auth/getUser"));
 
 // Logout
 router.post("/logout", AuthMiddleware, require("../controllers/auth/logout"));
