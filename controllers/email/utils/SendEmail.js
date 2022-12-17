b77866de34ee62840f275e4e464a8cab51d817f2
deep1358 <deepshah1358@gmail.com>
2022-12-17 14:33:32 +0530

    Return promise1

--- controllers/email/utils/SendEmail.js
+++ controllers/email/utils/SendEmail.js
@@ -8,61 +8,37 @@ module.exports = async (
     paymentModeWiseExpense
 ) =>
     new Promise(async (resolve, reject) => {
-        console.log("Sending email to: " + userEmail);
+        const transporter = nodemailer.createTransport({
+            auth: {
+                user: process.env.NODEMAILER_EMAIL_ID,
+                pass: process.env.NODEMAILER_EMAIL_PASSWORD,
+            },
+            host: "smtp.gmail.com",
+            secure: true, // use SSL
+        });
 
-        try {
-            const transporter = nodemailer.createTransport({
-                auth: {
-                    user: process.env.NODEMAILER_EMAIL_ID,
-                    pass: process.env.NODEMAILER_EMAIL_PASSWORD,
-                },
-                host: "smtp.gmail.com",
-                secure: true, // use SSL
-            });
-            const data = await new Promise((resolve, reject) => {
-                // verify connection configuration
-                console.log("in");
-                transporter.verify(function (error, success) {
-                    console.log(error, success);
-                    if (error) {
-                        console.log(error);
-                        reject(error);
-                    } else {
-                        console.log("Server is ready to take our messages");
-                        resolve(success);
-                    }
-                });
+        const messageOptions = {
+            from: `Expense Tracker <${process.env.NODEMAILER_EMAIL_ID}>`,
+            to: process.env.NODEMAILER_EMAIL_ID,
+            subject: `Expense Report for ${
+                months[new Date().getMonth() - 1]
+            } ${new Date().getFullYear()}`,
+            html: require("./utils/EmailTemplate")(
+                userEmail,
+                totalExpense,
+                categoryWiseExpense,
+                paymentModeWiseExpense
+            ),
+        };
 
-                console.log("out");
-            });
-
-            console.log(data);
-
-            const messageOptions = {
-                from: `Expense Tracker <${process.env.NODEMAILER_EMAIL_ID}>`,
-                to: process.env.NODEMAILER_EMAIL_ID,
-                subject: `Expense Report for ${
-                    months[new Date().getMonth() - 1]
-                } ${new Date().getFullYear()}`,
-                html: "Hey",
-            };
-
-            console.log(messageOptions);
-
-            console.log("start");
-            transporter.sendMail(messageOptions, function (error, info) {
-                console.log(info, error);
-                if (error) {
-                    console.log(error);
-                    reject(error);
-                } else {
-                    console.log(`Email has successfully sent!`);
-                    resolve(info);
-                }
-            });
-            console.log("end");
-        } catch (e) {
-            console.log("ERROR:", e);
-            reject(e);
-        }
+        transporter.sendMail(messageOptions, function (error, info) {
+            console.log(info);
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log(`Email has successfully sent!`);
+                resolve("Email has successfully sent!");
+            }
+        });
     });
