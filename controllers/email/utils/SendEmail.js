e765c867cb1568942d3034bdf6ec998481d1084c
deep1358 <deepshah1358@gmail.com>
2022-12-17 14:15:03 +0530

    Test email functionality2

--- controllers/email/utils/SendEmail.js
+++ controllers/email/utils/SendEmail.js
@@ -9,57 +9,63 @@ module.exports = async (
 ) => {
     console.log("Sending email to: " + userEmail);
 
-    const transporter = nodemailer.createTransport({
-        auth: {
-            user: process.env.NODEMAILER_EMAIL_ID,
-            pass: process.env.NODEMAILER_EMAIL_PASSWORD,
-        },
-        host: "smtp.gmail.com",
-        port: 465,
-        secureConnection: true,
-        secure: true, // use SSL
-    });
-
-    await new Promise((resolve, reject) => {
-        // verify connection configuration
-        transporter.verify(function (error, success) {
-            if (error) {
-                console.log(error);
-                reject(error);
-            } else {
-                console.log("Server is ready to take our messages");
-                resolve(success);
-            }
+    try {
+        const transporter = nodemailer.createTransport({
+            auth: {
+                user: process.env.NODEMAILER_EMAIL_ID,
+                pass: process.env.NODEMAILER_EMAIL_PASSWORD,
+            },
+            host: "smtp.gmail.com",
+            port: 465,
+            secureConnection: true,
+            secure: true, // use SSL
         });
-    });
+        const data = await new Promise((resolve, reject) => {
+            // verify connection configuration
+            console.log("in");
+            transporter.verify(function (error, success) {
+                console.log(error, success);
+                if (error) {
+                    console.log(error);
+                    reject(error);
+                } else {
+                    console.log("Server is ready to take our messages");
+                    resolve(success);
+                }
+            });
 
-    const messageOptions = {
-        from: `Expense Tracker <${process.env.NODEMAILER_EMAIL_ID}>`,
-        to: process.env.NODEMAILER_EMAIL_ID,
-        subject: `Expense Report for ${
-            months[new Date().getMonth() - 1]
-        } ${new Date().getFullYear()}`,
-        html: require("./EmailTemplate")(
-            totalExpense,
-            categoryWiseExpense,
-            paymentModeWiseExpense,
-            months
-        ),
-    };
-
-    console.log("start");
-    await new Promise((resolve, reject) => {
-        // send mail
-        transporter.sendMail(messageOptions, function (error, info) {
-            console.log(info, error);
-            if (error) {
-                console.log(error);
-                reject(error);
-            } else {
-                console.log(`Email has successfully sent!`);
-                resolve(info);
-            }
+            console.log("out");
         });
-    });
-    console.log("end");
+
+        console.log(data);
+
+        const messageOptions = {
+            from: `Expense Tracker <${process.env.NODEMAILER_EMAIL_ID}>`,
+            to: process.env.NODEMAILER_EMAIL_ID,
+            subject: `Expense Report for ${
+                months[new Date().getMonth() - 1]
+            } ${new Date().getFullYear()}`,
+            html: "Hey",
+        };
+
+        console.log(messageOptions);
+
+        console.log("start");
+        await new Promise((resolve, reject) => {
+            // send mail
+            transporter.sendMail(messageOptions, function (error, info) {
+                console.log(info, error);
+                if (error) {
+                    console.log(error);
+                    reject(error);
+                } else {
+                    console.log(`Email has successfully sent!`);
+                    resolve(info);
+                }
+            });
+        });
+        console.log("end");
+    } catch (e) {
+        console.log("ERROR:", e);
+    }
 };
